8357df938a59f9c2099080d0bd688c942eb1948b
elzanayufae <114554822+elzanayufae@users.noreply.github.com>
2023-02-23 15:10:54 +0700

    Add files via upload

---
+++ pertemuan1/array.php
@@ -0,0 +1,16 @@
+<?php
+// array indexing
+$animals = ['kucing', 'kambing', 'kerbau', 'singa'];
+
+// Mengakses array indexing
+echo $animals[1] . '<br>';
+
+// array assosiatif
+$student1 = [
+    'name' => 'elzan',
+    'nim' => 0110123456,
+    'major' => 'Information Systems'
+];
+
+// Mengakses array assosiatif
+echo $student1['major'];
---
+++ pertemuan1/tipe_data.php
@@ -0,0 +1,19 @@
+<?php
+// String 
+$name = "elzan";
+
+// Intreger
+$umur = 20;
+
+//Float
+$ipk = 3.8;
+
+//Boolean
+$isStudent = true;
+
+// Array dibahas terpisah
+
+// menampilkan tifedata dan nilainya
+var_dump($name);
+
+echo "<br> Nama saya $name Umur saya adalah $umur";
